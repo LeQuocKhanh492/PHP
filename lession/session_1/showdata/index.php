c1eca529132c31f01fe7a50b92946cc9246a755a
LeQuocKhanh492 <wwwlenguyen1@gmail.com>
2023-10-21 22:20:35 +0700

    update

---
+++ lession/session_1/showdata/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "<pre>";
+    $a = "dsad";
+    echo $a;
+    echo "<br>";
+    var_dump($a);
+    echo "<br>";
+    $arr =[1,3,4,5];
+    print_r($arr);
+    ?>
+</body>
+</html>
